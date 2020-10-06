bc0712015b39203ebe648b1c0d1cfa907a6e97cb
e0260222 <e0260222@u.nus.edu>
2020-10-06 19:19:01 +0800

    Add Encrypt/Decrypt kernels

--- js/kernels.js
+++ js/kernels.js
@@ -7,14 +7,18 @@ const gaussianBlur = document.getElementById('gaussian-blur');
 const invertColours = document.getElementById('invert-colours');
 const grayscaleColours = document.getElementById('grayscale-colours');
 const edgeDetection = document.getElementById('edge-detection');
+const encrypt = document.getElementById('encrypt');
+const decrypt = document.getElementById('decrypt');
 
 const enableGpu = document.getElementById('enable-gpu');
 const fpsNumber = document.getElementById('fps-number');
 const timeTaken = document.getElementById('time-taken');
+
 let lastCalledTime = Date.now();
 let fps;
 let delta;
 let dispose = setup();
+
 enableGpu.onchange = () => {
     if (dispose)
         dispose();
@@ -26,7 +30,6 @@ function setup() {
         mode: enableGpu.checked ? 'gpu' : 'cpu'
     });
 
-    // THIS IS THE IMPORTANT STUFF
     const horizontalFlipKernal = gpu.createKernel(function (frame, isEnabled) {
         const pixel = frame[this.thread.y][this.thread.x];
         if (isEnabled) {
@@ -99,7 +102,7 @@ function setup() {
             const k6 = 0;
             const k7 = -1;
             const k8 = 0;
-            if (this.thread.y > 0 && this.thread.y < 480 - 1 && this.thread.x < 640 - 1 && this.thread.x >0) {
+            if (this.thread.y > 0 && this.thread.y < 480 - 1 && this.thread.x < 640 - 1 && this.thread.x > 0) {
                 const a0 = frame[this.thread.y + 1][this.thread.x - 1];
                 const a1 = frame[this.thread.y + 1][this.thread.x];
                 const a2 = frame[this.thread.y + 1][this.thread.x + 1];
@@ -113,7 +116,7 @@ function setup() {
                     col[i] = a0[i] * k0 + a1[i] * k1 + a2[i] * k2 + a3[i] * k3 + a4[i]* k4 
                                 + a5[i] * k5 + a6[i] * k6 + a7[i] * k7 + a8[i] * k8;
                 }
-                this.color(col[0], col[1], col[2], 1);
+                this.color(col[0], col[1], col[2], pixel.a);
             } else {
                 this.color(pixel.r, pixel.g, pixel.b, pixel.a);
             }
@@ -131,30 +134,65 @@ function setup() {
         const pixel = frame[this.thread.y][this.thread.x];
         var col = [0, 0, 0];
         if (isEnabled) {
-            const k0 = 1 / 16;
-            const k1 = 2 / 16;
-            const k2 = 1 / 16;
-            const k3 = 2 / 16;
-            const k4 = 4 / 16;
-            const k5 = 2 / 16;
-            const k6 = 1 / 16;
-            const k7 = 2 / 16;
-            const k8 = 1 / 16;
-            if (this.thread.y > 0 && this.thread.y < 480 - 1 && this.thread.x < 640 - 1 && this.thread.x >0) {
-                const a0 = frame[this.thread.y + 1][this.thread.x - 1];
-                const a1 = frame[this.thread.y + 1][this.thread.x];
-                const a2 = frame[this.thread.y + 1][this.thread.x + 1];
-                const a3 = frame[this.thread.y][this.thread.x - 1];
-                const a4 = frame[this.thread.y][this.thread.x];
-                const a5 = frame[this.thread.y][this.thread.x + 1];
-                const a6 = frame[this.thread.y - 1][this.thread.x - 1];
-                const a7 = frame[this.thread.y - 1][this.thread.x];
-                const a8 = frame[this.thread.y - 1][this.thread.x + 1];
+            const k0 = 1 / 256;
+            const k1 = 4 / 256;
+            const k2 = 6 / 256;
+            const k3 = 4 / 256;
+            const k4 = 1 / 256;            
+            const k5 = 4 / 256;
+            const k6 = 16 / 256;
+            const k7 = 24 / 256;
+            const k8 = 16 / 256;
+            const k9 = 4 / 256;            
+            const k10 = 6 / 256;
+            const k11 = 24 / 256;
+            const k12 = 36 / 256;
+            const k13 = 24 / 256;
+            const k14 = 6 / 256;            
+            const k15 = 4 / 256;
+            const k16 = 16 / 256;
+            const k17 = 24 / 256;
+            const k18 = 16 / 256;
+            const k19 = 4 / 256;            
+            const k20 = 1 / 256;
+            const k21 = 4 / 256;
+            const k22 = 6 / 256;
+            const k23 = 4 / 256;
+            const k24 = 1 / 256;            
+            if (this.thread.y > 1 && this.thread.y < 480 - 2 && this.thread.x < 640 - 2 && this.thread.x > 1) {
+                const a0 = frame[this.thread.y + 2][this.thread.x - 2];
+                const a1 = frame[this.thread.y + 2][this.thread.x - 1];
+                const a2 = frame[this.thread.y + 2][this.thread.x];
+                const a3 = frame[this.thread.y + 2][this.thread.x + 1];
+                const a4 = frame[this.thread.y + 2][this.thread.x + 2];                
+                const a5 = frame[this.thread.y + 1][this.thread.x - 2];
+                const a6 = frame[this.thread.y + 1][this.thread.x - 1];
+                const a7 = frame[this.thread.y + 1][this.thread.x];
+                const a8 = frame[this.thread.y + 1][this.thread.x + 1];
+                const a9 = frame[this.thread.y + 1][this.thread.x + 2];                
+                const a10 = frame[this.thread.y][this.thread.x - 2];
+                const a11 = frame[this.thread.y][this.thread.x - 1];
+                const a12 = frame[this.thread.y][this.thread.x];
+                const a13 = frame[this.thread.y][this.thread.x + 1];
+                const a14 = frame[this.thread.y][this.thread.x + 2];                
+                const a15 = frame[this.thread.y - 1][this.thread.x - 2];
+                const a16 = frame[this.thread.y - 1][this.thread.x - 1];
+                const a17 = frame[this.thread.y - 1][this.thread.x];
+                const a18 = frame[this.thread.y - 1][this.thread.x + 1];
+                const a19 = frame[this.thread.y - 1][this.thread.x + 2];                
+                const a20 = frame[this.thread.y - 2][this.thread.x - 2];
+                const a21 = frame[this.thread.y - 2][this.thread.x - 1];
+                const a22 = frame[this.thread.y - 2][this.thread.x];
+                const a23 = frame[this.thread.y - 2][this.thread.x + 1];
+                const a24 = frame[this.thread.y - 2][this.thread.x + 2];
                 for (var i = 0; i < 3; i++) { // Compute the convolution for each of red [0], green [1] and blue [2]
                     col[i] = a0[i] * k0 + a1[i] * k1 + a2[i] * k2 + a3[i] * k3 + a4[i]* k4 
-                                + a5[i] * k5 + a6[i] * k6 + a7[i] * k7 + a8[i] * k8;
+                                + a5[i] * k5 + a6[i] * k6 + a7[i] * k7 + a8[i] * k8 + a9[i] * k9
+                                + a10[i] * k10 + a11[i] * k11 + a12[i] * k12 + a13[i] * k13 + a14[i] * k14
+                                + a15[i] * k15 + a16[i] * k16 + a17[i] * k17 + a18[i] * k18 + a19[i] * k19
+                                + a20[i] * k20 + a21[i] * k21 + a22[i] * k22 + a23[i] * k23 + a24[i] * k24;
                 }
-                this.color(col[0], col[1], col[2], 1);
+                this.color(col[0], col[1], col[2], pixel.a);
             } else {
                 this.color(pixel.r, pixel.g, pixel.b, pixel.a);
             }
@@ -170,17 +208,27 @@ function setup() {
     
     const edgeDetectionKernel = gpu.createKernel(function (frame, isEnabled) {
         const pixel = frame[this.thread.y][this.thread.x];
-        var col = [0, 0, 0];
+        var gx = [0, 0, 0];
+        var gy = [0, 0, 0];
         if (isEnabled) {
-            const k0 = -1;
-            const k1 = -1;
-            const k2 = -1;
-            const k3 = -1;
-            const k4 = 8;
-            const k5 = -1;
-            const k6 = -1;
-            const k7 = -1;
-            const k8 = -1;
+            const x0 = 1;
+            const x1 = 0;
+            const x2 = -1;
+            const x3 = 2;
+            const x4 = 0;
+            const x5 = -2;
+            const x6 = 1;
+            const x7 = 0;
+            const x8 = -1;
+            const y0 = 1;
+            const y1 = 2;
+            const y2 = 1;
+            const y3 = 0;
+            const y4 = 0;
+            const y5 = 0;
+            const y6 = -1;
+            const y7 = -2;
+            const y8 = -1;
             if (this.thread.y > 0 && this.thread.y < 480 - 1 && this.thread.x < 640 - 1 && this.thread.x >0) {
                 const a0 = frame[this.thread.y + 1][this.thread.x - 1];
                 const a1 = frame[this.thread.y + 1][this.thread.x];
@@ -192,10 +240,15 @@ function setup() {
                 const a7 = frame[this.thread.y - 1][this.thread.x];
                 const a8 = frame[this.thread.y - 1][this.thread.x + 1];
                 for (var i = 0; i < 3; i++) { // Compute the convolution for each of red [0], green [1] and blue [2]
-                    col[i] = a0[i] * k0 + a1[i] * k1 + a2[i] * k2 + a3[i] * k3 + a4[i]* k4 
-                                + a5[i] * k5 + a6[i] * k6 + a7[i] * k7 + a8[i] * k8;
+                    gx[i] = a0[i] * x0 + a1[i] * x1 + a2[i] * x2 + a3[i] * x3 + a4[i]* x4 
+                                + a5[i] * x5 + a6[i] * x6 + a7[i] * x7 + a8[i] * x8;
                 }
-                this.color(col[0], col[1], col[2], 1);
+                for (var i = 0; i < 3; i++) { // Compute the convolution for each of red [0], green [1] and blue [2]
+                    gy[i] = a0[i] * y0 + a1[i] * y1 + a2[i] * y2 + a3[i] * y3 + a4[i]* y4 
+                                + a5[i] * y5 + a6[i] * y6 + a7[i] * y7 + a8[i] * y8;
+                }
+                this.color(Math.sqrt(gx[0] * gx[0] + gy[0] * gy[0]), Math.sqrt(gx[1] * gx[1] + gy[1] * gy[1]),
+                            Math.sqrt(gx[2] * gx[2] + gy[2] * gy[2]) , pixel.a);
             } else {
                 this.color(pixel.r, pixel.g, pixel.b, pixel.a);
             }
@@ -209,6 +262,43 @@ function setup() {
         tactic: 'precision'
     });
             
+    const encryptKernel = gpu.createKernel(function (frame, key, isEnabled) {
+        const pixel = frame[this.thread.y][this.thread.x];
+        var col = [0,0,0];
+        if (isEnabled) {
+            for (var i=0; i < 3; i++) {
+                col[i] = Math.round(key[3 * i] * pixel.r * 255 + key[3 * i + 1] * pixel.g * 255 + key[3 * i + 2] * pixel.b * 255) % 256 / 255;
+	        }
+            this.color(col[0], col[1], col[2], pixel.a);
+        } else {
+            this.color(pixel.r, pixel.g, pixel.b, pixel.a);
+        }
+    }, {
+        pipeline: true,
+        output: [640, 480],
+        graphical: true,
+        tactic: 'precision'
+    });
+    
+    const decryptKernel = gpu.createKernel(function (frame, key, isEnabled) {
+        const pixel = frame[this.thread.y][this.thread.x];
+        var col = [0,0,0];
+        if (isEnabled) {
+            for (var i = 0; i < 3 ; i++) {
+                col[i] = Math.floor(key[3 * i] * pixel.r * 255 + key[3 * i + 1]
+                            * pixel.g * 255 + key[3 * i + 2] * pixel.b * 255) % 256 / 255;
+	        }
+            this.color(col[0], col[1], col[2], pixel.a);
+        } else {
+            this.color(pixel.r, pixel.g, pixel.b, pixel.a);
+        }
+    }, {
+        pipeline: true,
+        output: [640, 480],
+        graphical: true,
+        tactic: 'precision'
+    });
+    
     const textureToImageKernel = gpu.createKernel(function (frame) {
         const pixel = frame[this.thread.y][this.thread.x];
         this.color(pixel.r, pixel.g, pixel.b, pixel.a);
@@ -227,6 +317,7 @@ function setup() {
         
         if (enableGpu.checked){
             var t0 = performance.now();
+            
             const outputStage1 = horizontalFlipKernal(videoElement, horizontalFlip.checked);
             const outputStage2 = verticalFlipKernal(outputStage1, verticalFlip.checked);
             const outputStage3 = sharpenKernel(outputStage2, sharpen.checked);
@@ -234,11 +325,14 @@ function setup() {
             const outputStage5 = invertColourKernel(outputStage4, invertColours.checked);
             const outputStage6 = grayscaleColourKernal(outputStage5, grayscaleColours.checked);
             const outputStage7 = edgeDetectionKernel(outputStage6, edgeDetection.checked);
+            const outputStage8 = encryptKernel(outputStage7, getKey(0), encrypt.checked);
+            const outputStage9 = decryptKernel(outputStage8, getKey(1), decrypt.checked);
+            
+            textureToImageKernel(outputStage9);
+            
             var t1 = performance.now();
             var delta = t1 - t0;
-                    timeTaken.innerHTML = delta.toFixed(0);
-                    console.log(timeTaken.innerHTML);
-            textureToImageKernel(outputStage7);
+            timeTaken.innerHTML = delta.toFixed(3);
         } else {
             switch (getRadioValue()) {
                 case "horizontal-flip":
@@ -246,39 +340,58 @@ function setup() {
                     horizontalFlipKernal(videoElement, true);
                     var t1 = performance.now();
                     var delta = t1 - t0;
-                    timeTaken.innerHTML = delta.toFixed(0);
+                    timeTaken.innerHTML = delta.toFixed(3);
                     break;
                 case "vertical-flip":
+                    var t0 = performance.now();
                     verticalFlipKernal(videoElement, true);
+                    var t1 = performance.now();
+                    var delta = t1 - t0;
+                    timeTaken.innerHTML = delta.toFixed(3);
                     break;
                 case "sharpen":
+                    var t0 = performance.now();
                     sharpenKernel(videoElement, true);
+                    var t1 = performance.now();
+                    var delta = t1 - t0;
+                    timeTaken.innerHTML = delta.toFixed(3);
                     break;
                 case "gaussian-blur":
                     var t0 = performance.now();
                     gaussianBlurKernel(videoElement, true);
                     var t1 = performance.now();
                     var delta = t1 - t0;
-                    timeTaken.innerHTML = delta.toFixed(0);
+                    timeTaken.innerHTML = delta.toFixed(3);
                     break;
                 case "invert-colours":
+                    var t0 = performance.now();
                     invertColourKernel(videoElement, true);
+                    var t1 = performance.now();
+                    var delta = t1 - t0;
+                    timeTaken.innerHTML = delta.toFixed(3);
                     break;
                 case "grayscale-colours":
+                    var t0 = performance.now();
                     grayscaleColourKernal(videoElement, true);
+                    var t1 = performance.now();
+                    var delta = t1 - t0;
+                    timeTaken.innerHTML = delta.toFixed(3);
                     break;
                 case "edge-detection":
+                    var t0 = performance.now();
                     edgeDetectionKernel(videoElement, true);
+                    var t1 = performance.now();
+                    var delta = t1 - t0;
+                    timeTaken.innerHTML = delta.toFixed(3);
                     break;
                 case "no-filter":
                     var t0 = performance.now();
                     textureToImageKernel(videoElement);
                     var t1 = performance.now();
                     var delta = t1 - t0;
-                    timeTaken.innerHTML = delta.toFixed(0);
+                    timeTaken.innerHTML = delta.toFixed(3);
             }                        
-        }
-                
+        }    
         window.requestAnimationFrame(render);
         calcFPS();
     }
@@ -312,7 +425,6 @@ function calcFPS() {
 }
 
 function calcTimeTaken(delta) {
-    
     timeTaken.innerHTML = delta;
 }
 
@@ -333,4 +445,45 @@ function getRadioValue() {
             return elements[i].value;
         }
     } 
+}
+
+function getKey(type) {
+    var elements;
+    if (type == 0) {
+         elements = document.getElementsByName('encryption-key');   
+    } else {
+         elements = document.getElementsByName('decryption-key');   
+    }
+    var key;     
+    for(i = 0; i < elements.length; i++) { 
+        if(elements[i].checked) {
+            key = elements[i].value;
+        }
+    }
+    
+    return getKeyValue(key);
+}
+
+function getKeyValue(key) {
+    switch (key) {
+        case "ekey1":
+            keyValue = [6, 24, 1, 13, 16, 10, 20, 17, 15];
+            break;
+        case "ekey2":
+            keyValue = [14, 51, 1, 22, 75, 12, 3, 42, 12];
+            break;
+        case "ekey3":
+            keyValue = [212, 234, 111, 196, 67, 130, 93, 219, 8];
+            break;
+        case "dkey1":
+            keyValue = [118, 113, 224, 173, 118, 217, 5, 74, 104];
+            break;
+        case "dkey2":
+            keyValue = [212, 234, 111, 196,	67, 130, 93, 219, 8];
+            break;
+        case "dkey3":
+            keyValue = [14,	51,	1, 22, 75, 12, 3, 42, 12];
+            break;
+    }
+    return keyValue;
 }
